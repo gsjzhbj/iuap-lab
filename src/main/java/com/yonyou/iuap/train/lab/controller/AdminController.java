d0421bade31a73431f6a0f01368ae6ab235b1ef6
gaoshja <gaoshja@yonyou.com>
2020-03-16 14:38:02 +0800

    iuap培训实验室 login逻辑

--- src/main/java/com/yonyou/iuap/train/lab/controller/AdminController.java
+++ src/main/java/com/yonyou/iuap/train/lab/controller/AdminController.java
@@ -1,12 +1,17 @@
 package com.yonyou.iuap.train.lab.controller;
 
+import com.yonyou.iuap.train.lab.config.SystemConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminController {
+    Logger logger= LoggerFactory.getLogger(SystemConfig.SYSTEM_LOG_NAME);
     @RequestMapping("/admin")
     public String index(){
+        
         return "templates/admin/index";
     }
 }
